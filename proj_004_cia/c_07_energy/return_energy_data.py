6169fc7c10c00de105fbe7ddefef0e046aa28e5f
Bayo <bayoadesinaus@gmail.com>
2025-11-23 20:20:20 +0100

    small change in return energy

--- proj_004_cia/c_07_energy/return_energy_data.py
+++ proj_004_cia/c_07_energy/return_energy_data.py
@@ -144,12 +144,13 @@ def return_energy_data(
 # ---------------------------------------------------------------------------------------------------------------------
 if __name__ == '__main__':
     import platform
-    country = False
+    country = True
     # ----------------------------------------------------------------------------------------------------------------------------------
     if platform.system() == 'Windows':
         json_folder = r'C:\Users\bayoa\impact_projects\claude_solve_cia\proj_004_cia\_raw_data'
     else:
-        json_folder = os.path.join(os.path.dirname(os.path.dirname(__file__)), '_raw_data')
+        json_folder = os.path.join(os.path.dirname(
+            os.path.dirname(__file__)), '_raw_data')
     if country:
         region_folder = f'north-america'
         cia_code = 'us'
